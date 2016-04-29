a42cac5701188705ef1fbf2965244997366db207
Xavier Courangon <xavier.courangon-EXTERNAL@gemalto.com>
2016-04-29 14:40:21 +0200

    Add ContextRoot

---
+++ src/main/java/org/lolongo/ContextRoot.java
@@ -0,0 +1,38 @@
+package org.lolongo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Context Root for Context tree.
+ *
+ * @author Xavier Courangon
+ */
+public class ContextRoot extends ContextNode {
+
+    private static Logger logger = LoggerFactory.getLogger(ContextRoot.class);
+
+    public ContextRoot() {
+	super(null);
+    }
+
+    @Override
+    public ContextNode getParent() {
+	return null;
+    }
+
+    @Override
+    public boolean isRoot() {
+	return true;
+    }
+
+    @Override
+    public ContextRoot getRoot() {
+	return this;
+    }
+
+    @Override
+    public String getName() {
+	return "";
+    }
+}
