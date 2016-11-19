74099b1440f2e765ff2bdc8d9be4a98e2de9169d
Xavier Courangon <xavier.courangon@gmail.com>
2016-11-19 08:28:50 +0100

    Remove ContextRef concept from Context classes.

--- src/main/java/org/lolongo/ContextNode.java
+++ src/main/java/org/lolongo/ContextNode.java
@@ -1,91 +1,89 @@
 package org.lolongo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Context implementation for Context tree.
+ * A ContextNode as a Node in a hierarchical tree of Context.<
  * 
  * @author Xavier Courangon
  */
 public class ContextNode extends NamedContext {
 
-    private static Logger logger  = LoggerFactory.getLogger(ContextNode.class);
+    private static Logger logger = LoggerFactory.getLogger(ContextNode.class);
 
-    private ContextNode   parent = null;
-    private Map<String,ContextNode> subcontexts=new HashMap<>();
-    private boolean inherit=true;
-  
+    private ContextNode parent = null;
+    private Map<String, ContextNode> subcontexts = new HashMap<>();
+    private boolean inherit = true;
+
     public ContextNode(String id) {
-      super(id);
+        super(id);
     }
 
     public ContextNode(String id, boolean inherit) {
-      this(id);
-      this.inherit=inherit;
+        this(id);
+        this.inherit = inherit;
     }
-  
+
     public ContextNode getParent() {
         return parent;
     }
-  
-  public boolean isRoot() {
-    return parent==null;
-  }
+
+    public boolean isRoot() {
+        return parent == null;
+    }
 
     public Collection<ContextNode> getSubcontexts() {
-      return subcontexts.values();
-    }
-  
-    public ContextNode getSubcontext(String name) throws ContextNotFound {
-      if(subcontexts.containsKey(name)){
-        return subcontexts.get(name);
-      } else {
-        throw new ContextNotFound(name);
-      }
-    }
-  
-    @Override
-    public <T, R extends Ref<T>> T get(R ref) throws RefNotFound {
-      return get(ref,true);
+        return subcontexts.values();
     }
 
-      
+    public ContextNode getSubcontext(String name) throws ContextNotFound {
+        if (subcontexts.containsKey(name)) {
+            return subcontexts.get(name);
+        } else {
+            throw new ContextNotFound(name);
+        }
+    }
+
+    @Override
+    public <T, R extends Ref<T>> T get(R ref) throws RefNotFound {
+        return get(ref, true);
+    }
+
     public <T, R extends Ref<T>> T get(R ref, boolean recursive) throws RefNotFound {
         try {
             return super.get(ref);
         } catch (RefNotFound e) {
-            if (recursive && inherit && parent!=null) {
+            if (recursive && inherit && parent != null) {
                 try {
                     return parent.get(ref);
-                } catch(RefNotFound _) {
+                } catch (RefNotFound _) {
                 }
             }
             throw e;
         }
     }
 
-    public void addSubcontext(ContextNode subcontext) throws ContextAlreadyExists{
-      final String name=subcontext.getName();
-      if(subcontexts.containsKey(name)) {
-      	throw new ContextAlreadyExists(name);
-      } else {
-			subcontext.parent=this;
-      	subcontexts.put(name,subcontext);
-      }
+    public void addSubcontext(ContextNode subcontext) throws ContextAlreadyExists {
+        final String name = subcontext.getName();
+        if (subcontexts.containsKey(name)) {
+            throw new ContextAlreadyExists(name);
+        } else {
+            subcontext.parent = this;
+            subcontexts.put(name, subcontext);
+        }
     }
-  
+
     public ContextNode getRoot() {
         if (this.isRoot()) {
             return this;
         } else {
-          assert parent !=null;
-          return parent.getRoot();
+            assert parent != null;
+            return parent.getRoot();
         }
     }
 
@@ -103,9 +101,5 @@ public class ContextNode extends NamedContext {
         }
         return isParentOf(parent);
     }
-  
-  public Context getContext(String contextRef) throws ContextNotFound {
-    return ContextRef.getContext(this, contextRef);
-  }
 
 }
