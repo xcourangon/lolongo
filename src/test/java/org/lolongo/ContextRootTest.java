53e2caea49dea7946948a5835858e50a40e11ae5
Xavier Courangon <xavier.courangon@gmail.com>
2016-11-19 01:28:55 +0100

    Add tests for ContextRoot class.

---
+++ src/test/java/org/lolongo/ContextRootTest.java
@@ -0,0 +1,40 @@
+package org.lolongo;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ContextRootTest {
+
+	private ContextRoot contextRoot;
+
+	@Before
+	public void init() {
+		contextRoot = new ContextRoot();
+	}
+
+	@Test
+	public void testGetName() {
+		Assert.assertNull(contextRoot.getName());
+	}
+
+	@Test
+	public void testGetParent() {
+		Assert.assertNull(contextRoot.getParent());
+	}
+
+	@Test
+	public void testIsRoot() {
+		Assert.assertTrue(contextRoot.isRoot());
+	}
+
+	@Test
+	public void testGetRoot() {
+		Assert.assertEquals(contextRoot, contextRoot.getRoot());
+	}
+
+	@Test
+	public void testGetAbsoluteContext() {
+		Assert.assertEquals("/", ContextRef.getAbsoluteContext(contextRoot));
+	}
+}
